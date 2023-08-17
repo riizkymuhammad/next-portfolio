0b85b52d5a71d5fc5e76dba1e1d4c7f2b66dda29
Muhammad Rizky <mrizky98@gmail.com>
2023-08-17 23:45:40 +0700

    delete color-modes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Image from "next/image";
 import Link from "next/link";
-import colorModes from '../styles/color-modes.js'
 
 export default function Home() {
   return (
